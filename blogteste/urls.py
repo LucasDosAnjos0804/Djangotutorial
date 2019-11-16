789bd7c556651512442fe764c3b102d65e3efbb8
LucasDosAnjos0804 <anjoslucas7@gmail.com>
2019-11-16 16:21:02 -0400

    agora tem detalhes

--- blogteste/urls.py
+++ blogteste/urls.py
@@ -1,7 +1,14 @@
+"""
+    CONTROLA AS URL's E DESIGNA A "VIEW" CORRESPONDENTE
+"""
 from django.urls import path
 from . import views
 
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    ## O primeiro argumento se trata do padrão da url com nome post_detail, observe <int:pk> que dizer que o Django espera em seguidao do '/' um numero inteiro e irá trasnferi-lo para view(controlador) como uma variavel chamada pk.
+    ## O segundo argumento eh a funcao disparada em view.
+    ## O terceiro argumento eh o nome da view.
 ]
--- blogteste/views.py
+++ blogteste/views.py
@@ -1,8 +1,9 @@
 """
     SIMULA O CONTROLER, MAS TEM NOME DE VIEW
+    CONTROLA COMO AS INFORMACOES CAO EXIBIDAS E RECEBIDAS DOS "TEMPLATES"
 """
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
@@ -14,3 +15,9 @@ def post_list(request):
 
     #retorna render, funcao http, o diretorio do tamplate, mensagem ao template
     return render(request,'blogteste/post_list.html',{'posts':posts})
+
+def post_detail(request, pk):
+
+    post = get_object_or_404(Post, pk=pk)
+    
+    return render(request, 'blogteste/post_detail.html', {'post': post})
