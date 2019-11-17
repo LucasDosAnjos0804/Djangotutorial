169a7de9c4a09e0043fed21b846b39a36904c01d
LucasDosAnjos0804 <anjoslucas7@gmail.com>
2019-11-17 01:54:28 -0400

    agora altera na madrugada

--- blogteste/urls.py
+++ blogteste/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     ## O primeiro argumento se trata do padrão da url com nome post_detail, observe <int:pk> que dizer que o Django espera em seguidao do '/' um numero inteiro e irá trasnferi-lo para view(controlador) como uma variavel chamada pk.
     ## O segundo argumento eh a funcao disparada em view.
     ## O terceiro argumento eh o nome da view.
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
